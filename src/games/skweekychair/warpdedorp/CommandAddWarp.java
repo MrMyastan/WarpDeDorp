85e440a7a5214ac24417b5628cd151f5abca7fb9
Copeland Steele <admiral_gamer@outlook.com>
2020-12-06 01:13:39 -0600

    update error checking

--- src/games/skweekychair/warpdedorp/CommandAddWarp.java
+++ src/games/skweekychair/warpdedorp/CommandAddWarp.java
@@ -10,7 +10,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CommandAddWarp implements CommandExecutor {
+public class CommandAddWarp implements TabExecutor {
 	 
 	WarpDDPlugin mainInstance;
 	HashMap<String, Location> teleportLocations;
@@ -34,15 +34,27 @@ public class CommandAddWarp implements CommandExecutor {
 		}
 		
 		World world = Bukkit.getWorld(args[1]);
-		double xCord = Double.parseDouble(args[2]);
-		double yCord = Double.parseDouble(args[3]);
-		double zCord = Double.parseDouble(args[4]);
+		double xCord;
+		double yCord;
+		double zCord;
+		
+		try {
+		xCord = Double.parseDouble(args[2]);
+		yCord = Double.parseDouble(args[3]);
+		zCord = Double.parseDouble(args[4]);
+		} catch (NumberFormatException nfe) {return false;}
 		
 		Location warpPoint = new Location(world, xCord, yCord, zCord);
 		
 		if (args.length == 7) {
-			float yaw = Float.parseFloat(args[5]);
-			float pitch = Float.parseFloat(args[6]);
+			float yaw;
+			float pitch;
+			
+			try {
+			yaw = Float.parseFloat(args[5]);
+			pitch = Float.parseFloat(args[6]);
+			} catch (NumberFormatException nfe) {return false;}
+			
 			warpPoint.setYaw(yaw);
 			warpPoint.setPitch(pitch);
 		}
