60d738e9f4eba3911c302ef4b25a26d2e5acd019
Copeland Steele <admiral_gamer@outlook.com>
2020-12-06 00:23:11 -0600

    can specify coords, world, and look direction for addwarp

--- src/games/skweekychair/warpdedorp/CommandAddWarp.java
+++ src/games/skweekychair/warpdedorp/CommandAddWarp.java
@@ -2,7 +2,9 @@ package games.skweekychair.warpdedorp;
 
 import java.util.HashMap;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -20,6 +22,8 @@ public class CommandAddWarp implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if ((1 < args.length && args.length < 5) || args.length == 6) {return false;} 
+		
 		if (sender instanceof Player && args.length == 1) {
 	       	Player player = (Player) sender;
 	       	Location playerLoc = player.getLocation();
@@ -28,6 +32,25 @@ public class CommandAddWarp implements CommandExecutor {
 			mainInstance.saveConfig();
 			return true;
 		}
-		return false;
+		
+		World world = Bukkit.getWorld(args[1]);
+		double xCord = Double.parseDouble(args[2]);
+		double yCord = Double.parseDouble(args[3]);
+		double zCord = Double.parseDouble(args[4]);
+		
+		Location warpPoint = new Location(world, xCord, yCord, zCord);
+		
+		if (args.length == 7) {
+			float yaw = Float.parseFloat(args[5]);
+			float pitch = Float.parseFloat(args[6]);
+			warpPoint.setYaw(yaw);
+			warpPoint.setPitch(pitch);
+		}
+		
+       	teleportLocations.put(args[0], warpPoint);
+       	mainInstance.getConfig().set(args[0], warpPoint);
+		mainInstance.saveConfig();
+		
+		return true;
 	}
 }
